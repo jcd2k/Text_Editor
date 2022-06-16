d9fd233c81bdb4a39b8dd25cd328703980b394b4
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-16 15:05:52 -0600

    Added js page functionality

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readwrite');
+  const save = transaction.objectStore('jate');
+  const request = save.put({ id: 1, text: content });
+  const result = await request;
 
+  console.log(result, ' saved successfully');
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id) => {
+  
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readonly');
+  const save = transaction.objectStore('jate');
+  const request = save.get(id);
+  const result = await request;
+  
+  console.log(result, ' retrieved successfully');
+  
+  if(result) {
+    return result.text;
+  } else {
+    return;
+  }
+  
+}
 
 initdb();
