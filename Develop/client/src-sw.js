3a9117838f5ca5de37b8c877ce723eff0f1cae01
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-16 15:29:43 -0600

    modified service worker and webpack

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -28,5 +28,14 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
